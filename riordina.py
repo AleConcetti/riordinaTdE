526d742be2eb592284841225e9de799420c4b601
Ale.Concetti <ale.concetti@gmail.com>
2019-01-13 09:49:50 +0100

    script riordina

---
+++ riordina.py
@@ -0,0 +1,9 @@
+import os
+path='/home/alessandro/Politecnico/b) Magistrale - Computer Science and Engineering/2) Architettura del calcolatore e sistemi operativi/TE AXO'
+fileList=os.listdir(path)
+
+for file in fileList:
+    for year in range(2005,2020):
+        if str(year) in file:
+            renamedFile=str(year)+') '+ file
+            os.rename(os.path.join(path,file),os.path.join(path,renamedFile))
